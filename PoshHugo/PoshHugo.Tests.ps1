d5f75992b8032b42ffa0eb8d564ff6e71279595b
mattypenny <mattypenny@outlook.com>
2016-06-27 07:53:19 +0100

    Added some Pester Testering

---
+++ PoshHugo/PoshHugo.Tests.ps1
@@ -0,0 +1,134 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$TestData = "$here\PesterData"
+. "$here\$sut"
+
+Describe "PoshHugo" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
+
+
+Describe "get-HugoNameAndValue" {
+    It "returns name and value for a valid line" {
+        $Hugo = get-HugoNameAndValue -FrontMatterLine "Weighting: 103"
+        # [string]$name = $Hugo.name
+        # $name | Should Be 'Weighting'
+        $value = $Hugo.PropertyValue
+        $value | Should Be '103'
+        # "103" | Should Be '103'
+        # $Hugo.value | Should Be '103'
+    }
+
+}
+
+<#
+title: "10th June 1668 - Samuel Pepys visits Salisbury"
+description: ""
+lastmod: "2016-06-07"
+date: "2013-11-29"
+tags: [  ]
+categories: 
+ - "on-this-day"
+aliases: ["/on-this-day/june/10th-june-1668-samuel-pepys-visits-salisbury"]
+draft: No
+publishdate: "2013-11-29"
+weight: 610
+markup: "md"
+url: /on-this-day/june/10th-june-1668-samuel-pepys-visits-salisbury
+#>
+
+Describe "get-HugoContent" {
+
+    It "returns title" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $title = $HugoContent.title
+        $title | Should Be 'xx'
+    }
+
+    It "returns description" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $description = $HugoContent.description
+        $description | Should Be 'xx'
+    }
+
+    It "returns lastmod" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $lastmod = $HugoContent.lastmod
+        $lastmod | Should Be 'xx'
+    }
+
+    It "returns date" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $date = $HugoContent.date
+        $date | Should Be 'xx'
+    }
+
+    It "returns tags" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $tags = $HugoContent.tags
+        $tags | Should Be 'xx'
+    }
+
+    It "returns categories" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $categories = $HugoContent.categories
+        $categories | Should Be 'xx'
+    }
+
+    It "returns aliases" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $aliases = $HugoContent.aliases
+        $aliases | Should Be 'xx'
+    }
+
+    It "returns draft" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $draft = $HugoContent.draft
+        $draft | Should Be 'xx'
+    }
+
+    It "returns publishdate" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $publishdate = $HugoContent.publishdate
+        $publishdate | Should Be 'xx'
+    }
+
+    It "returns markup" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $markup = $HugoContent.markup
+        $markup | Should Be 'xx'
+    }
+
+    It "returns url" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $url = $HugoContent.url
+        $url | Should Be 'xx'
+    }
+
+    It "returns body" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $body = $HugoContent.body
+        $body | Should Be 'xx'
+    }
+
+    It "returns links" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $links = $HugoContent.links
+        $links | Should Be 'xx'
+    }
+
+    It "returns weighting" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $Weighting = $HugoContent.Weighting
+        $Weighting | Should Be '610'
+    }
+
+    It "returns images" {
+        $HugoContent = get-HugoContent -f $TestData\10th-june-1668-samuel-pepys-visits-salisbury.md 
+        $images = $HugoContent.images
+        $images | Should Be '610'
+    }
+
+}
---
+++ PoshHugo/PoshHugo.ps1
@@ -0,0 +1,296 @@
+﻿function append-NextAndPreviousLinksToMarkdownFile {
+<#
+.SYNOPSIS
+  Append next page and previous page links to Markdown Files based on the weighting
+.DESCRIPTION
+  Longer description
+
+.PARAMETER folder
+  Folder 
+
+.EXAMPLE
+
+
+
+
+#>
+  [CmdletBinding()]
+  Param( [string]$Folder = ".")
+  
+  write-startfunction
+  
+  $YamlFromAllTheFolders = foreach ($File in $Folder)
+  {
+      get-yaml $File
+  }
+  
+  $SortedYamlFromAllTheFolders = $YamlFromAllTheFolders | sort-object -property weighting, url
+
+  foreach ($Yaml in $SortedYamlFromAllTheFolders)
+  {
+    <# $File = $Yaml.????
+   
+    $PreviousNextText = "???"
+    
+    copy ??? ??? # take a backup
+    
+    "$PreviousText" >> $File
+    #>
+  }  
+  write-endfunction
+  return 
+}
+
+function get-HugoContent { 
+<#
+.SYNOPSIS
+    Return the HugoContent as an object
+.DESCRIPTION
+    Todo: get it to work for Json and Toml
+.PARAMETER HugoMarkdownFile
+
+.EXAMPLE
+     get-HugoFrontMatter -f D:\hugo\sites\example.com\content\on-this-day\10th-june-1668-samuel-pepys-visits-salisbury.md
+  title       :  "10th June 1668 - Samuel Pepys visits Salisbury"
+description :  ""
+lastmod     :  "2016-06-07"
+date        :  "2013-11-29"
+aliases     :  ["/on-this-day/june/10th-june-1668-samuel-pepys-visits-salisbury"]
+draft       :  No
+publishdate :  "2013-11-29"
+weight      :  610
+markup      :  "md"
+url         :  /on-this-day/june/10th-june-1668-samuel-pepys-visits-salisbury
+
+
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+    Param( [string][Alias ("f")]$HugoMarkdownFile = "$pwd"  ) 
+
+    write-startfunction
+
+    $Files = dir $HugoMarkDownFile
+
+    foreach ($File in $Files)
+    {
+        $MarkdownLines = select-string -pattern '^' $File
+
+        $NumberOfMarkDownLines = $MarkdownLines | measure-object -Line
+        write-debug "`$NumberOfMarkDownLines: $NumberOfMarkDownLines"
+
+    
+        $PropertyCount = 0
+        $YamlDividingLines = 0
+
+        # initialize the output values
+        $description = ""
+        $lastmod = ""
+        $date = ""
+        $tags = @{}
+        $categories = @{}
+        $aliases = ""
+        $draft = ""
+        $publishdate = ""
+        $weight = ""
+        $markup = ""
+        $url = ""
+        $content = ""
+           
+        # There are two passes through the $MarkdownLine. The first captures the
+        # 'simple' fields. The second captures fields which could have multiple 
+        # values, and possibly be split across multiple lines
+        #
+        # First Pass - simple values
+        foreach ($MarkdownLine in $MarkDownLines)
+        {
+            [string]$Line = $MarkdownLine.Line
+            if ($Line -like "---*")
+            {
+                $YamlDividingLines = $YamlDividingLines + 1
+            }
+            if ($YamlDividingLines -eq 1)
+            {
+
+                if ($Line -like " -*")
+                {
+                    write-debug "Childline: $Line"
+                }
+                
+                
+                $PropertyCount = $PropertyCount + 1
+            
+                $HugoNameAndValue = get-HugoNameAndOneLineValue -FrontMatterLine $Line
+                [string]$PropertyName = $HugoNameAndValue.name
+                [string]$PropertyValue = $HugoNameAndValue.Value
+
+                 
+                write-debug "`$PropertyName: <$PropertyName>"
+                write-debug "`$PropertyValue: <$PropertyValue>"
+               
+                switch ($PropertyName) 
+                {
+                    "title" 
+                    { 
+                        write-debug "in switch"; 
+                        $title = $PropertyValue 
+                    }
+                    "description"
+                    { 
+                        write-debug "description - in switch"; 
+                        $description = $PropertyValue 
+                    }
+                    "lastmod"
+                    { 
+                        write-debug "in switch"; 
+                        $lastmod = $PropertyValue 
+                    }
+                    "date"
+                    { 
+                        write-debug "in switch"; 
+                        $date = $PropertyValue 
+                    }
+                    "tags" 
+                    {
+                        $tags = convert-FromYaml???ToArray
+                        $TagString = $PropertyValue
+                        $TagString = $TagString.trimstart('[')
+                        $TagString = $TagString.trimend(']')
+                        $TagString = $TagString.trim()
+                        $TagArray = $TagString.split(',')
+                        
+                        write-debug "`$tagstring: <$tagstring>"
+                    
+                        $Element = -1
+                        $tags = foreach ($Tag in $TagArray)
+                        {
+                            $Element++
+                            $Tag = $Tag.trim()
+                            $Tag = $Tag.trim('"')
+                            write-debug "`$tag: <$tag>"
+                            $TagArray[$Element] = $Tag
+
+                        }
+#>
+                        $Tags = $TagArray
+                    }
+                    # aliases CAN be multiple, but I've not coded for this yet
+                    "aliases"
+                    { 
+                        write-debug "in switch"; 
+                        $aliases = $PropertyValue 
+                    }
+                    "draft"
+                    { 
+                        write-debug "in switch"; 
+                        $draft = $PropertyValue 
+                    }
+                    "publishdate"
+                    { 
+                        write-debug "in switch"; 
+                        $publishdate = $PropertyValue 
+                    }
+                    "weight"
+                    { 
+                        write-debug "in switch"; 
+                        $weight = $PropertyValue 
+                    }
+                    markup
+                    { 
+                        write-debug "in switch - markup"; 
+                        $markup = $PropertyValue 
+                    }
+                    "url"
+                    { 
+                        write-debug "in switch"; 
+                        $url = $PropertyValue 
+                    }  
+                    Default
+                    { 
+                        write-debug "Unknown property"
+                        $UnknownProperty = $PropertyValue 
+                    }  
+                }    
+            }
+            elseif ($YamlDividingLines -gt 1)
+            {
+                <#
+                    The front matter is over, so the rest is content
+                #>
+                write-debug "Adding to content"    
+
+            }
+        }
+
+
+
+        [PSCustomObject]@{
+            title = $title        
+            description = $description
+            lastmod = $lastmod
+            date = $date
+            tags = $tags
+            # # categories = $categories
+            # aliases CAN be multiple, but I've not coded for this yet
+            aliases = $aliases 
+            draft = $draft
+            publishdate = $publishdate
+            weight = $weight 
+            markup = $markup
+            url = $url
+            unknownproperty = $unknownproperty
+        }
+            
+    }
+    write-endfunction
+
+}
+
+set-alias temp get-template
+
+<#
+vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#>
+
+function get-HugoNameAndValue {
+
+<#
+.SYNOPSIS
+  Return property name and any value on the same line 
+.DESCRIPTION
+  
+
+.PARAMETER FrontMatterLine
+  Folder 
+
+.EXAMPLE
+#>
+  [CmdletBinding()]
+  Param( [string]$FrontMatterLine = ".")
+  
+  write-startfunction
+
+
+  $PositionOfFirstColon = $FrontMatterline.IndexOf(':')
+  if ($PositionOfFirstColon -eq -1)
+  {
+    $PositionOfFirstColon = 0
+  }
+
+  $PropertyName = $FrontMatterLine.Substring(0,$PositionOfFirstColon)
+                
+  $PropertyValue = $FrontMatterLine.Substring($PositionOfFirstColon +1  )
+            
+  $PropertyValue = $PropertyValue.trimstart(' ') 
+  $PropertyValue = $PropertyValue.trimend(' ') 
+  $PropertyValue = $PropertyValue.trimstart('"') 
+  $PropertyValue = $PropertyValue.trimend('"') 
+
+  [PSCustomObject]@{ 
+    PropertyName = $PropertyName
+    PropertyValue = $PropertyValue 
+  }
+
+}
+
